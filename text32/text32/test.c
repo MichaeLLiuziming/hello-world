0766f0d3e97b586b077ee7cf77b27ef74a48506e
Michael Liu <117092588+MichaeLLiuziming@users.noreply.github.com>
2022-12-13 19:41:04 +0800

    2022.12.13

---
+++ text32/text32/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{ 
+//	//字符指针
+//	char ch = 'w';
+//	char* p = &ch;
+//	const char* p2 = "abcdef";
+//	//指针数组-数组-存放指针的数组
+//	int* arr[10];
+//	char* ch[5];
+//	//数组指针-指向数组
+//	//int*p3;//整形指针--指向整型的指针
+//	//char*p4//字符指针-指向字符的
+//	int arr2[5];
+//	int(*p)[5] = &arr2;//取出数组的地址，pa就是一个数组指针 
+//	return 0;
+//}
+
+//数组指针--指向数组的指针
+//函数指针--指向函数的指针---存放函数地址的指针
+int main()
+{ 
+	return 0;
+}
