4a5cde510a88a19a4e8a080723eaf3534d2f8911
Michael Liu <117092588+MichaeLLiuziming@users.noreply.github.com>
2022-12-14 23:55:49 +0800

    2022.12.14

---
+++ text33/text33/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//char* my_strcpy(char* dest, const char* stc)
+//{
+//	return 0;
+//}
+//int main()
+//{
+//	char* (*p)(char*, const char*) = my_strcpy;
+//	char* (*pf[4])(char*, const char*) = { my_strcpy,my_strcpy ,my_strcpy ,my_strcpy };
+//	return 0; 
+//}  
+
+//#include<stdio.h>
+//int main() 
+//{
+//    char c1 = 'A', c2 = 'Y';
+//    printf("%d,%d\n", c1, c2);
+//    return 0;
+//}
+
+//using namespace std;
+//class SC
+//{
+//public:
+//    SC(int r) { R = _____________; }
+//    int Get() { return *R; };
+//private:
+//    int* R;
+//};
+//int main() 
+//{
+//    SC C(10);
+//    cout << C.Get() << endl;
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a, b;
+//    scanf("%d,%d", &a, &b);
+//    if (a > b) a = b;b = a;
+//    else a++£»b++;
+//    printf("%d,%d", a, b);
+//
+//}
